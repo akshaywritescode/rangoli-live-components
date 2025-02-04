cdfa6031268d76a6cdc72dc40845bcb5e4a139db
akshaywritescode <teamakshayyadav@gmail.com>
2025-02-04 10:37:55 +0530

    added theme switcher functionality

--- src/app/components/ThemeSwitcher.tsx
+++ src/app/components/ThemeSwitcher.tsx
@@ -2,22 +2,24 @@
 
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Laptop, MoonIcon, SunIcon } from "lucide-react";
+import { useTheme } from "next-themes"
 
-export default function ThemeSwitcher({defaultValue}: {defaultValue: "system" | "light" | "dark"}) {
+export default function ThemeSwitcher({ defaultValue }: { defaultValue: "system" | "light" | "dark" }) {
+    const { setTheme } = useTheme();
 
-  return (
-    <Tabs defaultValue={defaultValue}>
-      <TabsList>
-      <TabsTrigger value="system" /*onClick={() => setTheme("system")}*/>
-          <Laptop className="w-4" />
-        </TabsTrigger>
-        <TabsTrigger value="dark" /*onClick={() => setTheme("dark")}*/>
-          <MoonIcon className="w-4" />
-        </TabsTrigger>
-        <TabsTrigger value="light" /*onClick={() => setTheme("light")}*/>
-          <SunIcon className="w-4" />
-        </TabsTrigger>
-      </TabsList>
-    </Tabs>
-  );
+    return (
+        <Tabs defaultValue={defaultValue}>
+            <TabsList>
+                <TabsTrigger value="system" onClick={() => setTheme("system")}>
+                    <Laptop className="w-4" />
+                </TabsTrigger>
+                <TabsTrigger value="dark" onClick={() => setTheme("dark")}>
+                    <MoonIcon className="w-4" />
+                </TabsTrigger>
+                <TabsTrigger value="light" onClick={() => setTheme("light")}>
+                    <SunIcon className="w-4" />
+                </TabsTrigger>
+            </TabsList>
+        </Tabs>
+    );
 }
