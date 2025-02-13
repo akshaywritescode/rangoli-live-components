e9e31516e58fcc9c13d15b40b0bb05e6700bd795
akshaywritescode <teamakshayyadav@gmail.com>
2025-02-13 10:23:18 +0530

    updated separator component

--- src/app/components/Separator.tsx
+++ src/app/components/Separator.tsx
@@ -1,14 +1,22 @@
 "use client";
 
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
 type TSeparator = {
-  className?: string
-}
+  className?: string;
+};
 
 export default function Separator({ className }: TSeparator) {
+  const { theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
 
-  return (
-    <div
-      className={`${className} h-[1px]`}
-    ></div>
-  );
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  // Avoid rendering theme-based class until mounted
+  const themeClass = !mounted ? "" : theme === "light" ? "bg-black/10" : "bg-white/10";
+
+  return <div className={`${className} ${themeClass} h-[1px]`} />;
 }
--- src/app/s/separator/page.tsx
+++ src/app/s/separator/page.tsx
@@ -5,7 +5,7 @@ export default function separatorDemo() {
     return <div className="container-center">
         <Card className="w-[400px] h-[200px] p-5 flex flex-col justify-center items-center gap-3">
             <h1 className="text-sm">Look We are Separated by separator component</h1>
-            <Separator className="w-full bg-black/10" />
+            <Separator className="w-full" />
             <h1 className="text-sm">Look We are Separated by separator component</h1>
         </Card>
     </div>
