c27936e1153586a307bf0b8325daa2a0634dfb1d
akshaywritescode <teamakshayyadav@gmail.com>
2025-02-16 15:33:05 +0530

    added password input

---
+++ src/app/components/PasswordInput.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Input } from "@/components/ui/input";
+import { EyeClosed, EyeIcon } from "lucide-react";
+import { useState } from "react";
+
+type TPasswordInput = React.InputHTMLAttributes<HTMLInputElement> & {
+    className?: string;
+};
+
+export default function PasswordInput({ className, ...rest }: TPasswordInput) {
+    const [visibility, setVisibility] = useState(false);
+
+    return (
+        <div className="relative">
+            <Input
+                type={visibility ? "text" : "password"}
+                className={className}
+                {...rest} // Spread remaining props (like onClick, onChange, etc.)
+            />
+            {visibility ? (
+                <EyeIcon
+                    className="w-4 absolute right-5 top-1/2 -translate-y-1/2 text-black/40 cursor-pointer"
+                    onClick={() => setVisibility((prevState) => !prevState)}
+                />
+            ) : (
+                <EyeClosed
+                    className="w-4 absolute right-5 top-1/2 -translate-y-1/2 text-black/40 cursor-pointer"
+                    onClick={() => setVisibility((prevState) => !prevState)}
+                />
+            )}
+        </div>
+    );
+}
---
+++ src/app/p/password-input/page.tsx
@@ -0,0 +1,7 @@
+import PasswordInput from "@/app/components/PasswordInput";
+
+export default function PasswordInputDemo() {
+    return <div className="container-center">
+        <PasswordInput placeholder="Password" className="w-[300px]" />
+    </div>
+}
