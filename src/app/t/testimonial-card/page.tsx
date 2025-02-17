83164f12812c8b8649c74655f24c3427dd5a6e42
akshaywritescode <teamakshayyadav@gmail.com>
2025-02-17 17:57:29 +0530

    modiefied testimonial card

--- src/app/t/testimonial-card/page.tsx
+++ src/app/t/testimonial-card/page.tsx
@@ -4,6 +4,6 @@ import testimonialAvatar1 from "@/assets/avatar-1.png"
 
 export default function TestimonialCardDemo() {
     return <div className="container-center">
-        <TestimonialCard description="As a seasoned designer i always on the lookout for innovation tools, framer.com instatly grabbed my attention" testimonialAvatar={testimonialAvatar1} testimonialAvatarAlt="Avatar 1" testimonialName="Jammie Riveria" testimonialUserHandle="@jammietech123" />
+        <TestimonialCard description="Rangoli UI is a game-changer! The prebuilt components saved me hours of development time while maintaining top-notch design quality." testimonialAvatar={testimonialAvatar1} testimonialAvatarAlt="Avatar 1" testimonialName="Jammie Riveria" testimonialUserHandle="@jammietech123" />
     </div>
 }
